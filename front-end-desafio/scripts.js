c93f1762990fd99fdec9c7d80945a7ce76d6e9ca
Renan Rey <renan.reyc@gmail.com>
2020-06-25 10:44:50 -0300

    adicionado o botão de maximize na página de contéudos

--- front-end-desafio/scripts.js
+++ front-end-desafio/scripts.js
@@ -1,16 +1,25 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-const cards = document.querySelectorAll('.card')
+const modalOverlay = document.querySelector(".modal-overlay")
+const modal = document.querySelector(".modal")
+const cards = document.querySelectorAll(".card")
 
 for (let card of cards) {
     card.addEventListener("click", function() {
         const pageId = card.getAttribute("id")
-        modalOverlay.classList.add('active')
+        modalOverlay.classList.add("active")
         modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${pageId}`
     })
 }
 
 document.querySelector(".close-modal").addEventListener("click", function(){
-    modalOverlay.classList.remove('active')
+    modalOverlay.classList.remove("active")
     modalOverlay.querySelector("iframe").src = ""
+})
+
+document.querySelector(".maximize-modal").addEventListener("click", function(){
+    if(modal.classList.contains("maximize")){
+        modal.classList.remove("maximize")
+    }else {
+        modal.classList.add("maximize")
+    }
 })
 
