9d4e610fcf6d9c4f02f77ab3d89538a25a34e85b
Renan Rey <renan.reyc@gmail.com>
2022-02-03 00:26:38 -0300

    modulo 6 launchbase: criação de banco launchstore, layout basico da operacao, layout do cadastro e edição, cadastrando produtos e atualizando e excluindo produtos

---
+++ module06/public/scripts.js
@@ -0,0 +1,19 @@
+// Estudar Sobre Expressões Regulares Strings //
+
+
+const Mask = {
+    apply(input, func) {
+        setTimeout(function () {
+            input.value = Mask[func](input.value)
+        }, 1)
+    },
+    formatBRL(value) {
+        value = value.replace(/\D/g, "")
+
+        return value = new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value/100)
+    }
+}
+
